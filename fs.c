6b390c5466611de9b2dd46775575cee8491822b8
Mai Anh <anh.ttm193288@sis.hust.edu.vn>
2022-08-08 17:15:25 +0700

    done fs.c

--- fs.c
+++ fs.c
@@ -6,12 +6,34 @@
 
 #include "simplefs.h"
 
-/* Mount a simplefs partition */
+static struct file_system_type simplefs_file_system_type = {
+    .owner = THIS_MODULE,
+    .name = "simplefs",
+    .mount = simplefs_mount,
+    .kill_sb = simplefs_kill_sb,
+    .fs_flags = FS_REQUIRES_DEV,
+    .next = NULL,
+};
+
+/**
+ * @brief Mount a simplefs partition
+ * 
+ * @param fs_type describes the filesystem, partly initialized by the specific filesystem code
+ * @param flags mount flags
+ * @param dev_name the device name we are mounting
+ * @param data arbitrary mount options, usually comes as an ASCII string
+ * @return struct dentry* A dentry (short for "directory entry") is what the Linux kernel uses to keep track of 
+ * the hierarchy of files in directories. Each dentry maps an inode number to a file name and a parent directory.
+ */
 struct dentry *simplefs_mount(struct file_system_type *fs_type,
                               int flags,
                               const char *dev_name,
                               void *data)
 {
+    /**
+     * @brief Mount a filesystem residing on a block device
+     * 
+     */
     struct dentry *dentry =
         mount_bdev(fs_type, flags, dev_name, data, simplefs_fill_super);
     if (IS_ERR(dentry))
@@ -27,39 +49,32 @@ void simplefs_kill_sb(struct super_block *sb)
 {
     kill_block_super(sb);
 
-    pr_info("unmounted disk\n");
+    pr_info("Unmounted disk\n");
 }
-
-static struct file_system_type simplefs_file_system_type = {
-    .owner = THIS_MODULE,
-    .name = "simplefs",
-    .mount = simplefs_mount,
-    .kill_sb = simplefs_kill_sb,
-    .fs_flags = FS_REQUIRES_DEV,
-    .next = NULL,
-};
 
 static int __init simplefs_init(void)
 {
     int ret = simplefs_init_inode_cache();
     if (ret) {
-        pr_err("inode cache creation failed\n");
+        pr_err("Inode cache creation failed\n");
         goto end;
     }
 
+    /* Register a new filesystem */
     ret = register_filesystem(&simplefs_file_system_type);
     if (ret) {
-        pr_err("register_filesystem() failed\n");
+        pr_err("Register_filesystem() failed\n");
         goto end;
     }
 
-    pr_info("module loaded\n");
+    pr_info("Module loaded\n");
 end:
     return ret;
 }
 
 static void __exit simplefs_exit(void)
 {
+    /* Remove a file system that was previously successfully registered with the kernel. */
     int ret = unregister_filesystem(&simplefs_file_system_type);
     if (ret)
         pr_err("unregister_filesystem() failed\n");
